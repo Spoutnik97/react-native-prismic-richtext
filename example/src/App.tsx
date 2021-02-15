ccea89adbbaa48f896ab7f73daf46a6382a12a64
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2021-02-15 15:44:19 +0100

    :tada: (Provide a RichText component for Prismic content to React Native applications)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,12 +2,43 @@ import React, { useEffect, useState } from 'react'
 import { RichText } from 'react-native-prismic-richtext'
 import { RichTextContent } from '../../typings'
 
+const fakeContent = [
+  {
+    type: 'paragraph',
+    text:
+      'My new paragraph with fake content to display string words and other normal words',
+    spans: [
+      {
+        start: 0,
+        end: 16,
+        type: 'strong',
+      },
+    ],
+  },
+  {
+    type: 'list-item',
+    text: 'Wednesday – startup meeting',
+    spans: [
+      {
+        start: 0,
+        end: 27,
+        type: 'strong',
+      },
+    ],
+  },
+  {
+    type: 'paragraph',
+    text: 'the sentence of the end.',
+    spans: [],
+  },
+]
+
 const App = () => {
-  const [content, setContent] = useState<RichTextContent[]>([])
+  const [content, setContent] = useState<RichTextContent[]>(fakeContent)
   useEffect(() => {
     // fetch primsic richtext
     ;(async () => {
-      const fetchedContent: RichTextContent[] = []
+      const fetchedContent: RichTextContent[] = fakeContent
       setContent(fetchedContent)
     })()
   }, [])
